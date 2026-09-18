5c0ffd885b5d62dbc64a7f680bc5a02e630efa34
dani2318 <giacche.dani@gmail.com>
2026-09-18 19:48:02 +0200

    Revert "feat(server): add session idle cleanup loop"
    This reverts commit b0e2bc0f97cfe3ba69c2ac1a9737019d067f6390.

--- server/main.go
+++ server/main.go
@@ -4,10 +4,7 @@ import (
 	"flag"
 	"log"
 	"net"
-	"time"
 )
-
-var timeout = 60 * time.Second
 
 func main() {
 	// Define tcp and udp address
@@ -36,28 +33,14 @@ func main() {
 
 // serve runs the control and media loops until the listeners stop.
 func serve(ltcp net.Listener, pc *net.UDPConn) {
-	roomManager := NewRoomManager()
-	go readUDP(pc, roomManager)
-	go handle_sessions(roomManager)
+	roomManagaer := NewRoomManager()
+	go readUDP(pc, roomManagaer)
 	for {
 		conn, err := ltcp.Accept()
 		if err != nil {
 			log.Println(err)
 			return
 		}
-		go handleControl(conn, roomManager)
-	}
-}
-
-func handle_sessions(roomManager *RoomManager) {
-	for {
-		for _, r := range roomManager.rooms {
-			for _, s := range r {
-				if time.Since(s.lastSeen) > timeout {
-					// Handle of the session removal from map.
-				}
-			}
-		}
-		time.Sleep(timeout / 2)
+		go handleControl(conn, roomManagaer)
 	}
 }
